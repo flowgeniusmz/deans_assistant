3e161941904bf53df051f4f6ba6730f57ba3fd2c
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2024-02-29 09:17:21 -0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,8 +4,8 @@ from config import pagesetup as ps
 from app import display_title as disTitle, session_states as ss
 from streamlit_extras.stylable_container import stylable_container
 
-with open( "config/style.css" ) as css:
-    st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
+#with open( "config/style.css" ) as css:
+    #st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
 
 
 
