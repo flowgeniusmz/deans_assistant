3e69efc0cb8230919fb423a342e25e19ef029cab
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2024-02-28 23:26:38 -0600

    Update pagesetup.py

--- config/pagesetup.py
+++ config/pagesetup.py
@@ -49,6 +49,7 @@ def set_page_overview(varHeader, varText):
             """
             .stMarkdown {
                     padding-right: 1.5em;
+                    padding-left: 1.5em;
                 """]
         ):
                 set_blue_header(varHeader)
